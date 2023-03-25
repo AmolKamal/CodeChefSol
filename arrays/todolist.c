ae702c027393a9162b077e807641bb3f96c64943
Kamal <amol10kamal@gmail.com>
2023-03-25 21:39:57 +0530

    Added the solution to TODOLIST problem - 'Problems in your to-do list'

--- arrays/todolist.c
+++ arrays/todolist.c
@@ -1,2 +1,29 @@
 // This code is given by ~ Amol Kamal
 
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,count;
+	scanf("%d",&t);
+	
+	while(t--){
+	    count=0;
+	    int n;
+	    scanf("%d",&n);
+	    
+	    int ar[n];
+	    for(int i =0;i<n;i++){
+	        scanf("%d",&ar[i]);
+	    }
+	    
+	    for(int i=0;i<n;i++){
+	        if(ar[i]>=1000)
+	            count++;
+	    }
+	    printf("\n%d",count);
+	}
+
+	return 0;
+}
+
